718ddc489a61d808e6c130c5632df4c2d5631f37
Deepak Sharma <deepak@spectrocloud.com>
2021-08-19 11:28:05 +0530

    implement hub and conversion

---
+++ api/v1alpha3/conversion.go
@@ -0,0 +1,78 @@
+package v1alpha3
+
+import (
+	"github.com/spectrocloud/cluster-api-provider-maas/api/v1alpha4"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+func (in *MaasCluster) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha4.MaasCluster)
+
+	return Convert_v1alpha3_MaasCluster_To_v1alpha4_MaasCluster(in, dst, nil)
+}
+
+func (in *MaasCluster) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha4.MaasCluster)
+
+	return Convert_v1alpha4_MaasCluster_To_v1alpha3_MaasCluster(src, in, nil)
+}
+
+func (in *MaasClusterList) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha4.MaasClusterList)
+
+	return Convert_v1alpha3_MaasClusterList_To_v1alpha4_MaasClusterList(in, dst, nil)
+}
+
+func (in *MaasClusterList) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha4.MaasClusterList)
+
+	return Convert_v1alpha4_MaasClusterList_To_v1alpha3_MaasClusterList(src, in, nil)
+}
+
+func (in *MaasMachine) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha4.MaasMachine)
+
+	return Convert_v1alpha3_MaasMachine_To_v1alpha4_MaasMachine(in, dst, nil)
+}
+
+func (in *MaasMachine) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha4.MaasMachine)
+
+	return Convert_v1alpha4_MaasMachine_To_v1alpha3_MaasMachine(src, in, nil)
+}
+
+func (in *MaasMachineList) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha4.MaasMachineList)
+
+	return Convert_v1alpha3_MaasMachineList_To_v1alpha4_MaasMachineList(in, dst, nil)
+}
+
+func (in *MaasMachineList) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha4.MaasMachineList)
+
+	return Convert_v1alpha4_MaasMachineList_To_v1alpha3_MaasMachineList(src, in, nil)
+}
+
+func (in *MaasMachineTemplate) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha4.MaasMachineTemplate)
+
+	return Convert_v1alpha3_MaasMachineTemplate_To_v1alpha4_MaasMachineTemplate(in, dst, nil)
+}
+
+func (in *MaasMachineTemplate) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha4.MaasMachineTemplate)
+
+	return Convert_v1alpha4_MaasMachineTemplate_To_v1alpha3_MaasMachineTemplate(src, in, nil)
+}
+
+func (in *MaasMachineTemplateList) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha4.MaasMachineTemplateList)
+
+	return Convert_v1alpha3_MaasMachineTemplateList_To_v1alpha4_MaasMachineTemplateList(in, dst, nil)
+}
+
+func (in *MaasMachineTemplateList) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha4.MaasMachineTemplateList)
+
+	return Convert_v1alpha4_MaasMachineTemplateList_To_v1alpha3_MaasMachineTemplateList(src, in, nil)
+}
---
+++ api/v1alpha4/conversion.go
@@ -0,0 +1,13 @@
+package v1alpha4
+
+func (*MaasCluster) Hub() {}
+
+func (*MaasClusterList) Hub() {}
+
+func (*MaasMachine) Hub() {}
+
+func (*MaasMachineList) Hub()  {}
+
+func (*MaasMachineTemplate) Hub() {}
+
+func (*MaasMachineTemplateList) Hub() {}
