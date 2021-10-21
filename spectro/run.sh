2d4767703590d9815153d67e9d4af4bc59cd6ecb
Deepak Sharma <deepak@spectrocloud.com>
2021-10-21 10:54:12 +0530

    add spectro

---
+++ spectro/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#rm generated/*
+
+kustomize build --load_restrictor none global > generated/core-global.yaml
+kustomize build --load_restrictor none base > generated/core-base.yaml
+
