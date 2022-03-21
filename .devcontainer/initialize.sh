90570f1846f41341ee6da3336393f35013eccb2f
Saad Malik <saad@spectrocloud.com>
2022-03-21 04:32:52 +0000

    Looks good

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-sudo chown -R vscode $HOME/.minikube
-minikube start
+# sudo chown -R vscode $HOME/.minikube
+# minikube start
+# echo "start minikube"
+echo "start kind cluster"
+pwd
 
-echo DONE >> /tmp/foo
+# delete older (if running)
+kind delete cluster --name kind-maas
+
+# create kind
+kind create cluster --config .devcontainer/kind-cluster-with-extramounts.yaml --wait 300s
+
+# install docker
+clusterctl init --infrastructure docker
+
+# generate fun
+clusterctl generate cluster docker1 --infrastructure=docker --control-plane-machine-count=1 --worker-machine-count=1 --kubernetes-version=1.21.10 --flavor development > docker-simple.yaml
