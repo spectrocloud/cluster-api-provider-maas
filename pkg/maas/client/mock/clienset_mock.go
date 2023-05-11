69f1575df9992a4ecea9097fb8d11782724d9318
Deepak Sharma <deepak@spectrocloud.com>
2023-05-11 12:05:21 +0530

    make generate v1beta1

--- pkg/maas/client/mock/clienset_mock.go
+++ pkg/maas/client/mock/clienset_mock.go
@@ -135,6 +135,20 @@ func (mr *MockClientSetInterfaceMockRecorder) ResourcePools() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ResourcePools", reflect.TypeOf((*MockClientSetInterface)(nil).ResourcePools))
 }
 
+// SSHKeys mocks base method.
+func (m *MockClientSetInterface) SSHKeys() maasclient.SSHKeys {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SSHKeys")
+	ret0, _ := ret[0].(maasclient.SSHKeys)
+	return ret0
+}
+
+// SSHKeys indicates an expected call of SSHKeys.
+func (mr *MockClientSetInterfaceMockRecorder) SSHKeys() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SSHKeys", reflect.TypeOf((*MockClientSetInterface)(nil).SSHKeys))
+}
+
 // Spaces mocks base method.
 func (m *MockClientSetInterface) Spaces() maasclient.Spaces {
 	m.ctrl.T.Helper()
@@ -391,6 +405,20 @@ func (m *MockMachine) OSSystem() string {
 func (mr *MockMachineMockRecorder) OSSystem() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OSSystem", reflect.TypeOf((*MockMachine)(nil).OSSystem))
+}
+
+// PowerManagerOn mocks base method.
+func (m *MockMachine) PowerManagerOn() maasclient.PowerManagerOn {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "PowerManagerOn")
+	ret0, _ := ret[0].(maasclient.PowerManagerOn)
+	return ret0
+}
+
+// PowerManagerOn indicates an expected call of PowerManagerOn.
+func (mr *MockMachineMockRecorder) PowerManagerOn() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PowerManagerOn", reflect.TypeOf((*MockMachine)(nil).PowerManagerOn))
 }
 
 // PowerState mocks base method.
