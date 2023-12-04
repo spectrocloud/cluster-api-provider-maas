7000a1eacd30e41f5213efca947c1961fb8c0bf1
Amit Sahastrabuddhe <33931378+AmitSahastra@users.noreply.github.com>
2023-12-04 17:23:38 +0530

    Maas custom endpoint support (#114)

--- pkg/maas/scope/cluster.go
+++ pkg/maas/scope/cluster.go
@@ -23,6 +23,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 	infrav1beta1 "github.com/spectrocloud/cluster-api-provider-maas/api/v1beta1"
+	infrautil "github.com/spectrocloud/cluster-api-provider-maas/pkg/util"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
