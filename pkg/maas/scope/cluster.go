d904d163633808353e998bff52bc6671ccdd6e02
Amit Sahastrabuddhe <amit.sahastra@gmail.com>
2024-01-30 11:37:56 +0530

    Get custom api server port

--- pkg/maas/scope/cluster.go
+++ pkg/maas/scope/cluster.go
@@ -120,10 +120,7 @@ func (s *ClusterScope) Close() error {
 // APIServerPort returns the APIServerPort to use when creating the load balancer.
 func (s *ClusterScope) APIServerPort() int {
 	if infrautil.IsCustomEndpointPresent(s.MaasCluster.GetAnnotations()) {
-		if s.Cluster.Spec.ClusterNetwork != nil && s.Cluster.Spec.ClusterNetwork.APIServerPort != nil {
-			*s.Cluster.Spec.ClusterNetwork.APIServerPort = int32(s.MaasCluster.Spec.ControlPlaneEndpoint.Port)
-			return s.MaasCluster.Spec.ControlPlaneEndpoint.Port
-		}
+		return s.MaasCluster.Spec.ControlPlaneEndpoint.Port
 	}
 
 	if s.Cluster.Spec.ClusterNetwork != nil && s.Cluster.Spec.ClusterNetwork.APIServerPort != nil {
