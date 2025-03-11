a17c34b62f20d7cc02edd09e3c589981e897045d
Jayesh Srivastava <jayesh0200@gmail.com>
2025-03-12 01:02:50 +0530

    Added Spectro manifests

---
+++ spectro/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+#rm generated/*
+
+kustomize build --load-restrictor LoadRestrictionsNone global > generated/core-global.yaml
+kustomize build --load-restrictor LoadRestrictionsNone base > generated/core-base.yaml
